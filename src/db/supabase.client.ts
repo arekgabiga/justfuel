8f49d266c68cecf814e787612aa50e23308cd641
Arek Gabiga <arkadiusz.gabiga@smartrecruiters.com>
2025-10-20 13:30:13 +0200

    Added default user id

--- src/db/supabase.client.ts
+++ src/db/supabase.client.ts
@@ -6,3 +6,5 @@ const supabaseUrl = import.meta.env.SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.SUPABASE_KEY;
 
 export const supabaseClient = createClient<Database>(supabaseUrl, supabaseAnonKey);
+
+export const DEFAULT_USER_ID = "5cd0bbbb-cf97-45e1-959b-ee2f63af69a7";
